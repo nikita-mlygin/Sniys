013a6a919e80306e0a2d3fa36a0ae3d0b3f4af67
nikita-mlygin <nikita.malygin2017@mail.ru>
2023-09-22 11:29:59 +0300

    Add actor moving

--- Assets/Scripts/Actor/ActorPlayer.cs
+++ Assets/Scripts/Actor/ActorPlayer.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class ActorPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float speed;
+
+    private Vector2 direction = Vector2.zero;
+
+    private void Start()
     {
-        
+       rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition((Vector2)transform.position + direction.normalized * speed * Time.fixedDeltaTime);
     }
 }
