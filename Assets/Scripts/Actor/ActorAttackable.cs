717667ce0b0ff86c94206e9fc5257ebb51746893
nikita-mlygin <nikita.malygin2017@mail.ru>
2023-09-24 10:39:15 +0300

    finaly

--- Assets/Scripts/Actor/ActorAttackable.cs
+++ Assets/Scripts/Actor/ActorAttackable.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ActorAttackable : Attackable
 {
     public override void GetAttack(float damage, GameObject attacker)
     {
+        SceneManager.LoadScene("MainMenu");
         GameObject.Destroy(gameObject);
     }
 }
